72ff5a4eafbe3a05ade6a15f7eaf8e95e88a59fc
alex <rojkov@gmail.com>
2016-10-04 18:49:01 -0700

    update readme for baratine run

--- service/src/react/CommentService.java
+++ service/src/react/CommentService.java
@@ -6,17 +6,17 @@ import java.util.List;
 import io.baratine.service.OnInit;
 import io.baratine.service.Result;
 import io.baratine.service.Service;
-import io.baratine.web.http.Body;
-import io.baratine.web.http.Get;
-import io.baratine.web.http.Path;
-import io.baratine.web.http.Post;
-import io.baratine.web.http.Query;
+import io.baratine.web.Body;
+import io.baratine.web.Get;
+import io.baratine.web.Path;
+import io.baratine.web.Post;
+import io.baratine.web.Query;
 
 /**
  * Service providing a list of Heroes
  */
 @Service()
-@Path("/api")
+//@Path("/api")
 public class CommentService
 {
   private List<Comment> _comments = new ArrayList<>();
